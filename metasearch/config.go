d3c68d52e487577ee95015cba173b2f39b032cd4
Bohdan Bashynskyi <bohdanbashynskyi@bohdans-macbook-pro-1.home>
2024-12-05 15:52:09 +0100

    Fixed read config

---
+++ metasearch/config.go
@@ -0,0 +1,48 @@
+package metasearch
+
+import (
+	"fmt"
+	"os"
+
+	"go.uber.org/zap"
+	"gopkg.in/yaml.v2"
+	"storj.io/storj/satellite/metainfo"
+)
+
+const (
+	defaulConfigPath = "/root/.local/share/storj/metasearch/config.yaml"
+	defaultEndpoint  = ":9998"
+)
+
+type Config struct {
+	Database string
+	Metainfo metainfo.Config
+	Log      zap.Config
+	Endpoint string
+}
+
+func (c *Config) Read() {
+	c.Log = zap.NewDevelopmentConfig()
+	yamlData, err := os.ReadFile(defaulConfigPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	c.Endpoint = defaultEndpoint
+
+	err = yaml.Unmarshal(yamlData, c)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if c.Database == "" {
+		c.Database = os.Getenv("STORJ_DATABASE")
+	}
+
+	if c.Metainfo.DatabaseURL == "" {
+		c.Metainfo.DatabaseURL = os.Getenv("STORJ_METAINFO_DATABASE_URL")
+	}
+
+	if c.Endpoint == "" {
+		c.Endpoint = os.Getenv("STORJ_METASEARCH_ENDPOINT")
+	}
+}
